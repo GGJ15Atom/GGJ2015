df4f650f0baa9c681c8e38c8d97cc55812897bd2
Fanakartal - AsusPc <fanakartal@gmail.com>
2015-01-23 23:11:35 +0200

    menu ekranlari kabaca yapildi.

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+
+    //public GameObject button1;
+    //public GameObject button2;
+    //public GameObject button3;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    
+	}
+    
+    public void LoadANewLevel()
+    {
+        if(gameObject.tag == "PlayGame")
+            Application.LoadLevel(1);
+
+        if(gameObject.tag == "Credits")
+            Application.LoadLevel(2);
+
+        if (gameObject.tag == "Quit")
+            Application.Quit();
+
+        if (Application.loadedLevel == 2)
+            Application.LoadLevel(0);
+    }
+}
