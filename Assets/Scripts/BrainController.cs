f88d03478322f616264a86d15f9689437c7872ba
Fanakartal - AsusPc <fanakartal@gmail.com>
2015-01-24 15:28:08 +0200

    brain hareketi bitti.

--- Assets/Scripts/BrainController.cs
+++ Assets/Scripts/BrainController.cs
@@ -11,9 +11,13 @@ public class BrainController : MonoBehaviour {
     }
 
     public CellLine state;
-    private float speed = 2.0f;
-    private float minimum = 5.0F;
-    private float maximum = 10.0F;
+    //private float speed = 2.0f;
+    
+
+    void Awake()
+    {
+        //newPosition = transform.position;
+    }
     
     // Use this for initialization
 	void Start () 
@@ -25,6 +29,10 @@ public class BrainController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
+        Vector3 positionDown = new Vector2(transform.position.x, 0);
+        Vector3 positionMiddle = new Vector2(transform.position.x, 2);
+        Vector3 positionUp = new Vector2(transform.position.x, 4);
+        
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             if (state == CellLine.Down)
@@ -35,17 +43,16 @@ public class BrainController : MonoBehaviour {
             {
                 // go Down State
                 state = CellLine.Down;
-                transform.position = new Vector2(transform.position.x, 0);
-                
-                //this.transform.position.x += this.speed * Time.deltaTime;
-
-                //transform.position = Vector2.Lerp(new Vector2(transform.position.x, 2.0f),new Vector2(transform.position.x, 0.0f), (Time.time));
+                rigidbody2D.velocity = new Vector2(8.0f, -8.0f);
+                //transform.position = positionDown;   
+               
             }
             else if ((state == CellLine.Up))
             {
                 // go Middle State
                 state = CellLine.Middle;
-                transform.position = new Vector2(transform.position.x, 2);
+                //transform.position = positionMiddle;
+                rigidbody2D.velocity = new Vector2(8.0f, -8.0f);
             }
         }
 
@@ -55,18 +62,61 @@ public class BrainController : MonoBehaviour {
             {
                 // go Middle State
                 state = CellLine.Middle;
-                transform.position = new Vector2(transform.position.x, 2);
+                //transform.position = positionMiddle;
+                rigidbody2D.velocity = new Vector2(8.0f, 8.0f);
             }
             else if ((state == CellLine.Middle))
             {
                 // go Up State
                 state = CellLine.Up;
-                transform.position = new Vector2(transform.position.x, 4);
+                //transform.position = positionUp;
+                rigidbody2D.velocity = new Vector2(8.0f, 8.0f);
             }
             else if ((state == CellLine.Up))
             {
                 // do nothing
             }
         }
+
+        if (state == CellLine.Down && transform.position.y < 0.0f)
+            rigidbody2D.velocity = new Vector2(8.0f, 0.0f);
+
+        else if (state == CellLine.Middle && transform.position.y > 1.9f && transform.position.y < 2.1f)
+            rigidbody2D.velocity = new Vector2(8.0f, 0.0f);
+
+        else if (state == CellLine.Up && transform.position.y > 4.0f)
+            rigidbody2D.velocity = new Vector2(8.0f, 0.0f);
 	}
+
+    /*private IEnumerator ChangePathRoutine(Vector2 targetPosition)
+    {
+        //while (Vector2.Distance(transform.position, targetPosition) > 0.05f)
+        //{
+            //transform.position = Vector2.Lerp(transform.position, targetPosition, Time.fixedDeltaTime * speed);
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, Time.fixedDeltaTime * speed);
+
+            yield return null;
+        //}
+    }*/
 }
+
+
+
+//while(transform.position.y != 0)
+//    rigidbody2D.velocity = new Vector2(2.0f, -2.0f);
+
+//rigidbody2D.velocity = new Vector2(8.0f, 0.0f);
+
+
+//newPosition = positionDown;
+//transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * speed);
+
+
+//StartCoroutine(ChangePathRoutine(new Vector2(transform.position.x, 0)));
+
+//transform.position = Vector2.MoveTowards(transform.position, positionDown, Time.fixedDeltaTime * speed);
+
+//rigidbody2D.velocity = new Vector2(8.0f, 0.0f);
+
+//rigidbody.velocity = Vector3.Lerp(rigidbody2D.velocity, newVelocity, Time.deltaTime * speed);
